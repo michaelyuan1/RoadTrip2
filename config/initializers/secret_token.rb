d3aa630f985325c59f7cb45f829b68c462ce85f8
Michael Yuan <michaelyuan1@yahoo.com>
2011-12-25 10:50:01 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RoadTrip::Application.config.secret_token = '0e6f975806ad41c796d9e3085890728e05848afd2716d79390000d7b213cc212355f6c0a09baf220a6df41fc23c843d881de8d39cfd614344e1dbea41b68fbdb'
