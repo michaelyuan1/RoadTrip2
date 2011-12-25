b6690c4d610de704f899dc7db9b30d9331a49615
Michael Yuan <michaelyuan1@yahoo.com>
2011-12-25 11:17:08 -0800

    looks like sampleapp

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 RoadTrip::Application.routes.draw do
+  
+  root :to => "pages#home"
+  
+  match '/contact', :to => 'pages#contact'
+
+	match '/about', :to => 'pages#about'
+
+ 	match '/help',    :to => 'pages#help'
+	
+	match '/signup',    :to => 'users#new'
+
+	match '/signin',    :to => 'pages#signin'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
