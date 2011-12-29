2794e0946ed687931f44138f043ddbf93700ce05
Michael Yuan <michaelyuan1@yahoo.com>
2011-12-28 17:16:20 -0800

    signup page done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,21 @@
 class UsersController < ApplicationController
   def new
-    #@user=User.find(params[:id])
+    @user = User.new
     @title = "Sign Up"
   end
   def show
     @user = User.find(params[:id])
     #params is equivalent to args in java
+  end
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      @title = "Sign Up"
+      render 'new'
+    end
   end
   def edit
     
